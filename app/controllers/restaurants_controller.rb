6dbb06174550918347b6dd5033507610b6d35887
douglas teixeira silva <douglasts@outlook.com>
2024-11-20 20:23:02 -0300

    atualizado2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
  class RestaurantsController < ApplicationController
+before_action :set_restaurants, only: %i[show edit update destroy]
 
     def index
         
@@ -18,15 +19,40 @@
     
     def create
         @restaurant = Restaurant.new(restaurant_params)
-        @restaurant.save
-        
-        redirect_to restaurant_path(@restaurant)
+        if @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+          else
+            render :new, status: :unprocessable_entity
+          end
     end
+
+    def edit
+    end
+
+    def update
+        @restaurant.update(restaurant_params)
+    
+        redirect_to restaurant_path(@restaurant)
+      end
+    
+      def destroy
+        @restaurant.destroy
+    
+        redirect_to restaurants_path
+      end
+
 
     private
 
     def restaurant_params
         params.require(@restaurant).permit(:name, :address, :phone_number, :category)
+    end
+
+
+    def set_restaurants
+    
+        @restaurant = Restaurant.find(params[:id])
+    
     end
 
  end
